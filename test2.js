c8fe497a724bbf069890164b7a27b1ed6b7e4f5f
Janet Okedoyin <okedoyinjanet@gmail.com>
2025-03-31 21:33:58 +0100

    test2.js

--- test2.js
+++ test2.js
@@ -1,1 +1,7 @@
 console.log("Hey, I'm writing Javascript!")
+let myName = "John Doe"
+let myAge = 30  
+let myHobby = "coding"
+let myFavoriteColor = "blue"
+let myFavoriteFood = "pizza"
+console.log(myName, myAge, myHobby, myFavoriteColor, myFavoriteFood)
